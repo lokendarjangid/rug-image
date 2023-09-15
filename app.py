d0d9d6cacd545bde1f72a46440ee221c2ef7ab99
lokendar jangid <lokendarjangid1234@gmail.com>
2023-09-15 21:31:28 +0530

    removed debug

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
